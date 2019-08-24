0bf325e3730abd27f544ac64da81213dc9cf86a3
scott <scottarthurgall@gmail.com>
2019-08-24 14:25:33 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+  el: '.app',
+  data: {
+    product: `boots`,
+    description: `they're made for walking`,
+    image: './assets/vmSocks-green.jpg',
+    link: 'http://www.scott-gall.com',
+    inventory: 33
+  }
+})
