ae7cc9b2c780cc40cdcc0eec4a054170582e9b8f
scott <scottarthurgall@gmail.com>
2019-08-26 10:14:34 -0500

    added form

--- main.js
+++ main.js
@@ -39,67 +39,154 @@ Vue.component('product', {
                 :disabled="!inStock"
                 :class="{ disabledButton: !inStock }">Add to Cart</button>
 
-        <div class="cart">
-          <p>Cart({{cart}})</p>
-        </div>
-
       </section>
+
+      <div>
+        <h2>Reviews</h2>
+        <p v-if="!reviews.length">There are no reviews yet.</p>
+        <ul>
+          <li v-for="review in reviews">
+            <p>{{ review.name }}</p>
+            <p>Rating: {{ review.rating }}</p>
+            <p>{{ review.review }}</p>
+          </li>
+        </ul>
+      </div>
+
+      <product-review @review-submitted="addReview"></product-review>
     </div>
   `,
   data() {
     return  {
-        brand: 'Supreme',
-        product: `Socks`,
-        description: `they're made for walking`,
-        selectedVariant: 0,
-        details: ["cotton", "polyester", "gender-neutral"],
-        variants: [
-          {
-            variantId: 2234,
-            variantColor: "green",
-            variantImage: "./assets/vmSocks-green.jpg",
-            variantQuantity: 10
-          },
-          {
-            variantId: 6666,
-            variantColor: "blue",
-            variantImage: "./assets/vmSocks-blue.jpg",
-            variantQuantity: 0
-          },
-        ],
-        cart: 0,
-      }
+      brand: 'Supreme',
+      product: `Socks`,
+      description: `they're made for walking`,
+      selectedVariant: 0,
+      details: ["cotton", "polyester", "gender-neutral"],
+      variants: [
+        {
+          variantId: 2234,
+          variantColor: "green",
+          variantImage: "./assets/vmSocks-green.jpg",
+          variantQuantity: 10
+        },
+        {
+          variantId: 6666,
+          variantColor: "blue",
+          variantImage: "./assets/vmSocks-blue.jpg",
+          variantQuantity: 0
+        },
+      ],
+      reviews: []
+    }
+  },
+  methods: {
+    addToCart() {
+      this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId)
     },
-    methods: {
-      addToCart() {
-        this.cart += 1;
-      },
-      updateProduct(index) {
-        this.selectedVariant = index;
-      }
+    updateProduct(index) {
+      this.selectedVariant = index;
     },
-    computed: {
-      title() {
-        return `${this.brand} ${this.product}`;
-      },
-      image() {
-        return this.variants[this.selectedVariant].variantImage
-      },
-      inStock() {
-        return this.variants[this.selectedVariant].variantQuantity
-      },
-      shipping() {
-        if (this.premium) {
-          return "Free"
-        } 
-        return 2.99
+    addReview(productReview) {
+      this.reviews.push(productReview)
+    }
+  },
+  computed: {
+    title() {
+      return `${this.brand} ${this.product}`;
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity;
+    },
+    shipping() {
+      if (this.premium) {
+        return "Free";
+      } 
+      return 2.99;
+    }
+  }
+});
+
+Vue.component('product-review', {
+  template: `
+    <form class="review-form" @submit.prevent="onSubmit">
+      <p v-if="errors.length">
+        <b>Please correct the following error(s):</b>
+        <ul>
+          <li v-for="error in errors">{{ error }}</li>
+        </ul>
+      </p>
+
+      <p>
+        <label for="name">Name:</label>
+        <input id="name" v-model="name">
+      </p>
+
+      <p>
+        <label for="review">Review:</label>
+        <textarea id="review" v-model="review"></textarea>
+      </p>
+
+      <p>
+        <label for="rating">Rating:</label>
+        <select id="rating" v-model.number="rating">
+          <option>5</option>
+          <option>4</option>
+          <option>3</option>
+          <option>2</option>
+          <option>1</option>
+        </select>
+      </p>
+
+      <p>
+        <input type="submit" value="Submit">
+      </p>
+
+    </form>
+  `,
+  data() {
+    return {
+      name: null,
+      review: null,
+      rating: null,
+      errors: []
+    }
+  },
+  methods: {
+    onSubmit() {
+      if (this.name && this.review && this.rating) {
+        let productReview = {
+          name: this.name,
+          review: this.review,
+          rating: this.rating
+        };
+        this.$emit('review-submitted', productReview);
+        this.name = null;
+        this.review = null;
+        this.rating = null;
+      }
+      else {
+        if(!this.name) this.errors.push("Name required.");
+        if(!this.review) this.errors.push("Review required.");
+        if(!this.rating) this.errors.push("Rating required.");
       }
     }
-  }); 
+  }
+});
 
 var app = new Vue({
   el: '.app',
   data: {
-    premium: false 
+    premium: false,
+    cart: [],
+  },
+  methods: {
+    updateCart(id) {
+      this.cart.push(id);
+    }
   }
-})
+});
+
